bfd29dfe5d9fac9963b6351ebbbd7ac94527ff56
Christhian Montes Aguilar <39229428+stevenmontes@users.noreply.github.com>
2023-07-08 11:30:59 -0600

    80. Validar que sean únicos los nombre

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -6,6 +6,8 @@ export const GifExpertApp = () => {
     const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
 
     const onAddCategory = (newCategory) => {
+        if (categories.includes(newCategory)) return;
+
         setCategories([newCategory, ...categories]);
     }
 
