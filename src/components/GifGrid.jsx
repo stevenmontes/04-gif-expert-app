78fcb12e0a375938079aeb83156b214432331612
Christhian Montes Aguilar <39229428+stevenmontes@users.noreply.github.com>
2023-07-11 21:59:17 -0600

    82. Fetch API - Obtener las imágenes deseadas Part 2

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,17 +1,6 @@
 import React from 'react'
+import { getGifs } from '../helpers/getGifs'
 
-const getGifs = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=5mxc7zj14aUGvpomc7lnb8Qep6mSSNmf&q=${category}&limit=20`
-    const response = await fetch(url);
-    const { data = [] } = await response.json();
-
-    const gifs = data.map(img => ({
-        id: img.id,
-        title: img.title,
-        url: img.images.downsized_medium.url
-    }))
-    console.log(gifs);
-}
 export const GifGrid = ({ category }) => {
 
 
