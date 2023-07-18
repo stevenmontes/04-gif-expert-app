b31994c6a7d75286b52d0591c9e207746751a0c9
Christhian Montes Aguilar <39229428+stevenmontes@users.noreply.github.com>
2023-07-17 18:57:17 -0600

    87. Custom Hook - useFetchGifs

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,19 +1,8 @@
-import { useEffect, useState } from 'react';
-import { getGifs } from '../helpers/getGifs'
 import { GifItem } from './GifItem';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 
 export const GifGrid = ({ category }) => {
-
-    const [images, setImages] = useState([]);
-
-    const getImages = async () => {
-        const newImages = await getGifs(category);
-        setImages([...newImages]);
-    }
-
-    useEffect(() => {
-        getImages();
-    }, []);
+    const { images, isLoading } = useFetchGifs(category)
 
     return (
         <>
