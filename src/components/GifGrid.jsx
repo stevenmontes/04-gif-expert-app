1d45bfed379319e4aec2bb061f2355acd4aebe37
Christhian Montes Aguilar <39229428+stevenmontes@users.noreply.github.com>
2023-07-18 19:33:11 -0600

    88. Mostrar mensaje de carga

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -8,6 +8,10 @@ export const GifGrid = ({ category }) => {
         <>
             <h3>{category}</h3>
 
+            {
+                isLoading && (<h2>Cargando...</h2>)
+            }
+
             <div className='card-grid'>
                 {
                     images.map((image) =>
